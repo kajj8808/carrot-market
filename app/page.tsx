5e0d3d9c706eb17ef2ed12fd8f7d8c03f9e3258a
kajj8808 <a01039388808@gmail.com>
2024-04-03 04:34:57 +0900

    #3.2 card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex items-center justify-center h-screen px-5 bg-gray-300">
+      <div className="w-full p-5 bg-white shadow-md rounded-2xl">
+        <div className="flex items-center justify-between ">
+          <div className="flex flex-col">
+            <span className="-mb-1 font-semibold text-gray-600">
+              In transit
+            </span>
+            <span className="text-4xl font-semibold ">Coolblue</span>
+          </div>
+          <div className="bg-orange-400 rounded-full size-12">
+            <div />
+          </div>
+        </div>
+        <div className="flex items-center gap-2 my-2">
+          <span className="px-2.5 py-1.5 text-sm font-medium text-white uppercase bg-green-400 rounded-full">
+            Today
+          </span>
+          <span>90:30-10:30</span>
+        </div>
+        <div className="relative">
+          <div className="absolute w-full h-2 bg-gray-200 rounded-full" />
+          <div className="absolute w-2/3 h-2 bg-green-400 rounded-full" />{" "}
+        </div>
+        <div className="flex items-center justify-between mt-5 text-gray-600">
+          <span>Expected</span>
+          <span>Sorting center</span>
+          <span>In transit</span>
+          <span className="text-gray-400">Delivered</span>
+        </div>
+      </div>
+    </main>
+  );
 }
