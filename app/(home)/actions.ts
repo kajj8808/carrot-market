da8bca9f02f2c402a2e49fdf793d2ccb145d8f4c
kajj8808 <a01039388808@gmail.com>
2024-08-07 02:59:41 +0900

    🐛 비밀번호 1234 -> 12345

--- app/(home)/actions.ts
+++ app/(home)/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 export default async function handleForm(prevState: any, formData: FormData) {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  if (formData.get("password") === "1234") {
+  if (formData.get("password") === "12345") {
     return {
       ok: true,
     };
